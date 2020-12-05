4f6e7e7a300e236536411abf16b0c4819df7bf74
Zim <zimtsui@gmail.com>
2020-12-05 18:09:24 +0800

    dev

--- dist/adaptor.js
+++ dist/adaptor.js
@@ -1,20 +1,22 @@
 function adaptor(service) {
     service.start(err => {
-        if (err) {
+        if (err)
             console.error(err);
-            service.stop().then(() => {
-                process.exitCode = 0;
-            }, err => {
-                console.error(err);
-                process.exitCode = 1;
-            }).then(() => {
-                setTimeout(() => void process.exit(), 1000).unref();
-            });
-        }
+        service.stop().then(() => {
+            process.exitCode = 0;
+        }, err => {
+            console.error(err);
+            process.exitCode = 1;
+        }).then(() => {
+            setTimeout(() => void process.exit(), 1000).unref();
+        });
+    }).catch(err => {
+        console.error(err);
+        service.stop().catch(() => { });
     });
     process.once('SIGINT', () => {
         process.once('SIGINT', () => void process.exit(1));
-        service.stop().catch(err => console.error(err));
+        service.stop().catch(() => { });
     });
 }
 export { adaptor as default, adaptor, };
--- src/adaptor.ts
+++ src/adaptor.ts
@@ -2,24 +2,25 @@ import { StartableLike } from './startable';
 
 function adaptor(service: StartableLike) {
     service.start(err => {
-        if (err) {
+        if (err) console.error(err);
+        service.stop().then(() => {
+            process.exitCode = 0;
+        }, err => {
             console.error(err);
-            service.stop().then(() => {
-                process.exitCode = 0;
-            }, err => {
-                console.error(err);
-                process.exitCode = 1;
-            }).then(() => {
-                setTimeout(
-                    () => void process.exit(),
-                    1000,
-                ).unref();
-            });
-        }
+            process.exitCode = 1;
+        }).then(() => {
+            setTimeout(
+                () => void process.exit(),
+                1000,
+            ).unref();
+        });
+    }).catch(err => {
+        console.error(err);
+        service.stop().catch(() => { });
     });
     process.once('SIGINT', () => {
         process.once('SIGINT', () => void process.exit(1));
-        service.stop().catch(err => console.error(err));
+        service.stop().catch(() => { });
     });
 }
 
