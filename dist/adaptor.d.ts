c1c07e8dc9ea655434d65507f9ec725f40ab0a95
Zim <zimtsui@gmail.com>
2021-05-01 15:00:22 +0800

    dist excluded

--- dist/adaptor.d.ts
+++
@@ -1,3 +0,0 @@
-import { StartableLike } from './startable';
-declare function adaptor(daemon: StartableLike, startTimeout?: number, stopTimeout?: number): void;
-export { adaptor as default, adaptor, };
