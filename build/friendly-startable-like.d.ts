d4221302de00d6bbdf8a1e685118fff60a949461
zim <zimtsui@gmail.com>
2022-06-12 18:53:02 +0800

    cleaned

--- build/friendly-startable-like.d.ts
+++
@@ -1,9 +0,0 @@
-import { StartableLike, RawStart, RawStop } from './startable-like';
-import { StateLike } from './state-like';
-export interface FriendlyStartableLike extends StartableLike {
-    setState(state: StateLike): void;
-    getState(): StateLike;
-    rawStart: RawStart;
-    rawStop: RawStop;
-}
-export declare const FriendlyStartableLike: {};
--- build/startable-like.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.StartableLike = void 0;
-exports.StartableLike = {};
-//# sourceMappingURL=startable-like.js.map
